c1f375bffa40a63ba111debf461bd43180001b0a
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2023-12-02 00:45:27 +0800

    format code

--- develop/index.tsx
+++ develop/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { createRoot } from "react-dom/client";
 import {
   Router,
@@ -18,21 +18,31 @@ function App() {
   const driver = useBrowserDriver();
   const nav = useNavigatorRef();
   return (
-    <div>
+    <Fragment>
       <Router driver={driver} navigator={nav}>
         <ul>
           <li>
-            <button onClick={() => nav.current?.push("/page/1")}>page1</button>
+            <button
+              className="btn"
+              onClick={() => nav.current?.push("/page/1")}
+            >
+              page1
+            </button>
           </li>
           <li>
-            <button onClick={() => nav.current?.push("/page/2")}>page2</button>
+            <button
+              className="btn"
+              onClick={() => nav.current?.push("/page/2")}
+            >
+              page2
+            </button>
           </li>
         </ul>
         <Route pattern="/page/:id/(.*)">
           <Page />
         </Route>
       </Router>
-    </div>
+    </Fragment>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 module.exports = {
-  content: ["./develop/**/*.{html,js}"],
+  content: ["./develop/**/*.{html,js,ts,tsx}"],
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dark", "retro"],
+  },
 }
