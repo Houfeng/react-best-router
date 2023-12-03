c1a3f14827c338994beb61f691726cba5a64f0d1
zhanfeng.hzf <zhanfeng.hzf@alibaba-inc.com>
2023-12-03 13:51:57 +0800

    docs

--- develop/index.tsx
+++ develop/index.tsx
@@ -1,7 +1,7 @@
 import "./index.css";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Route, Router, useBrowserDriver } from "../src";
+import { Route, Router, useHashDriver } from "../src";
 import { Layout } from "./Layout";
 import { SideBar } from "./SideBar";
 import { Header } from "./Header";
@@ -9,7 +9,7 @@ import { Footer } from "./Footer";
 import { Content } from "./Content";
 
 function RBRDocsAPP() {
-  const driver = useBrowserDriver();
+  const driver = useHashDriver();
   return (
     <Router driver={driver}>
       <Layout header={<Header />} footer={<Footer />} sideBar={<SideBar />}>
